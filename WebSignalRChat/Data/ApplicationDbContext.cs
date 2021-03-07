68c79211c9e309f1e883b46d1e1f92b2fcb9794b
Antonio Restrepo <jheanpiertony@gmail.com>
2021-03-07 17:43:28 -0500

    cambios desde pc personal

---
+++ WebSignalRChat/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSignalRChat.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
